dbf772393f3dc2352a765d5f2ee0277607dd9b60
Craigen1 <pmarkjethro@gmail.com>
2023-04-07 14:48:01 +0800

    update landing page

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,5 +1,4 @@
-<body class="body bg-white dark:bg-[#0F172A] min-h-screen leading-normal tracking-normal text-gray-900" style="font-family: 'Source Sans Pro', sans-serif;">
-
+<body class="bg-[url('cool-background1.png')] bg-fixed bg-cover bg-no-repeat" style="font-family: 'Source Sans Pro', sans-serif;">
 
   <div class="h-screen bg-slate-white">
     <!--Header-->
@@ -12,36 +11,36 @@
     </div>
   
     <!--Main--> 
-    <div class="container py-10 w-full px-6 mx-auto flex flex-col md:flex-row place-content-center">
+    <div class="container py-20 w-full px-6 mx-auto flex flex-col md:flex-row place-content-center">
   
       <!--Left Col-->
       <div class="pt-1 flex flex-col w-full xl:w-2/5 justify-center overflow-y-hidden">
-        <h1 class="my-4 text-3xl md:text-5xl text-blue-700 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">Receipt Generator App</h1>
+        <h1 class="my-4 text-3xl md:text-5xl text-blue-700 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">Receipt Generator App with Inventory Management</h1>
         <p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">Your Personal Online Business Companion</p>
   
         <div class="flex w-full justify-center md:justify-start lg:pb-0 fade-in">
           <div class="pl-6 w-50 card rounded-box place-items-center">
-            <p class="pb-3">Aldready have an account?</p>
-            <button class="btn border-0 w-40 bg-blue-700 hover:bg-blue-500">Sign In</button>
+            <p class="pb-3">Already have an account?</p>
+            <a href="/signin"><button class="btn border-0 w-40 bg-blue-700 hover:bg-blue-500">Sign In</button></a>
             </div> 
             <div class="divider lg:divider-horizontal">
             </div> 
             <div class="pb-2 w-50 card border-color-black-50 rounded-box place-items-center">
               <p class="pb-3">Don't have an account?</p>
-              <button class="btn border-0 w-40 bg-blue-700 hover:bg-blue-500">Sign Up</button>
+              <a href="/signup"><button class="btn border-0 w-40 bg-blue-700 hover:bg-blue-500">Sign Up</button></a>
             </div>
         </div>
       </div>
       <!--Right Col-->
         <div class="pl-40">
-          <div class="block max-w-sm rounded-lg bg-blue-500 shadow-lg dark:bg-neutral-700">
+          <div class="block max-w-sm rounded-lg bg-white bg-opacity-10 backdrop-blur-sm drop-shadow-lg shadow-lg dark:bg-neutral-700">
 
           <div class="p-6">
             <h5
-              class="mb-3 text-2xl font-bold leading-tight text-sky-100 dark:text-neutral-50">
+              class="mb-3 text-2xl font-bold leading-tight text-blue-700 dark:text-neutral-50">
               What is it?
             </h5>
-            <p class="mb-4 text-base text-slate-300 dark:text-sky-200">
+            <p class="mb-4 text-base text-white dark:text-sky-200">
               Receipt Generator is a web-based application with the primary goal of providing efficient and reliable transcript of transaction
               made to online businesses around the Philippines. The generator app includes a variety of features that may be useful.
               online sellers to track their sales as easily as possible! What exactly are you waiting for?
@@ -51,16 +50,10 @@
         </div>
         </div>
         </div>
- 
-      
-      <!--Footer-->
-      <div class="bottom-full pt-16 pb-6 text-sm text-center md:text-left fade-in">
-        <p class="text-gray-500 no-underline hover:no-underline"></p>
-      </div>
-      
-    </div>
-  
-
-  
   </body>
+
+  <!-- Footer Mentions -->
+<div class="fixed bottom-5 w-full text-center text-gray-400">
+  <p class="text-sm">Copyright &copy; Bytes 2023</p>
+</div>
   
