e83bc735f9c89f19d82d04afca994753911631f1
na14n <106856208+na14n@users.noreply.github.com>
2023-04-10 23:57:58 +0800

    profile page linked

--- app/src/routes/app/+layout.svelte
+++ app/src/routes/app/+layout.svelte
@@ -1,19 +1,14 @@
 <script>
     import { page } from '$app/stores'
 
-    // {id: 1, i: 'fa-solid fa-house', url: '/dashboard', title: 'Dashboard'},
-    // {id: 2, i: 'fa-solid fa-folder-open', url: '/inventory', title: 'Inventory'},
-    // {id: 3, i: 'fa-solid fa-receipt', url: '/receipt', title: 'Receipt'},
-    // {id: 4, i: 'fa-solid fa-circle-user', url: '/profile', title: 'Profile'},
-
     const navigation = [
         {
-            title: 'Dashboard',
+            title: 'dashboard',
             href: '/app/dashboard',
             i: 'fa-solid fa-house'
         },
         {
-            title: 'Products',
+            title: 'products',
             href: '/app/inventory',
             i: 'fa-solid fa-folder-open'
         },
@@ -53,16 +48,25 @@
 
 
             </div>
-    
-            <div class="w-max -mb-3">
-              <form action="/logout" method="POST" class="group flex items-center space-x-4 rounded-md px-4 pb-12 text-gray-600">
-                <button>
-                  <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 group-hover:fill-cyan-600" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
-                    <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
-                  </svg>
+            
+
+
+            <div class="w-max mb-3">
+              <div class="w-max mb-3">
+                <a
+                        href="/profile"
+                        class="flex shrink-0 justify-center items-center space-x-4 px-4 py-4 mb-3 text-slate-400 text-xl
+                        hover:text-blue-600 dark:text-slate-600
+                        "
+                        >
+                      <i class="fa-solid fa-circle-user"></i>
+                      </a>
+              </div>
+              <form action="/logout" method="POST" class="group flex items-center space-x-4 rounded-md px-4 pb-12 text-gray-600 ">
+                <button class="hover:text-blue-600">
+                    <i class="fa-solid fa-right-from-bracket"></i>
                 </button>
-                </form>
+              </form>
             </div>
             
           </div>
--- app/src/routes/profile/+layout.svelte
+++ app/src/routes/profile/+layout.svelte
@@ -3,6 +3,24 @@
     import { page } from '$app/stores'
 
 
+    const pages = [
+        {
+            title: 'dashboard',
+            href: '/app/dashboard',
+            i: 'fa-solid fa-house'
+        },
+        {
+            title: 'products',
+            href: '/app/inventory',
+            i: 'fa-solid fa-folder-open'
+        },
+        {
+            title: 'receipt',
+            href: '/app/receipt',
+            i: 'fa-solid fa-receipt'
+        },
+    ]
+
     const navigation = [
         {
             title: 'My Profile',
@@ -22,7 +40,55 @@
 
 <body class = "body bg-white dark:bg-[#0F172A] min-h-screen overflow-hidden">
 
-    <Sidebar />
+    <div id="sidebar" class="min-h-screen bg-slate-100 dark:bg-[#1E293B] fixed pt-7">
+        <div class="sidebar h-screen w-[3.35rem]">
+          <div class="flex h-screen flex-col justify-between pt-2 pb-6">
+            <div>
+              <div class="w-max p-2.5">
+                <img src="" class="" alt="">
+              </div>
+    
+              <div class="mt-6 space-y-2">
+                <div class="min-w-max">
+                  {#each pages as navItem}
+                    <a
+                      href="{navItem.href}"
+                      class="flex shrink-0 justify-center items-center space-x-4 px-4 py-4 mb-3 text-slate-400 text-xl
+                      hover:text-blue-600 dark:text-slate-600
+                      {$page.url.pathname == navItem.href ? 'text-slate-50 bg-gradient-to-r from-cyan-500 to-blue-500 hover:text-slate-50' : '' }"
+                      >
+                    <i class="{navItem.i}"></i>
+                    </a>
+                  {/each}
+                </div>
+              </div>
+
+
+            </div>
+            
+
+
+            <div class="w-max mb-3">
+              <div class="w-max mb-3">
+                <a
+                        href="/profile"
+                        class="flex shrink-0 justify-center items-center space-x-4 px-4 py-4 mb-3 text-slate-50 bg-gradient-to-r from-cyan-500 to-blue-500 hover:text-slate-50 text-xl
+                        hover:text-blue-600 dark:text-slate-600
+                        "
+                        >
+                      <i class="fa-solid fa-circle-user"></i>
+                      </a>
+              </div>
+                <form action="/logout" method="POST" class="group flex items-center space-x-4 rounded-md px-4 pb-12 text-gray-600 ">
+                    <button class="hover:text-blue-600">
+                        <i class="fa-solid fa-right-from-bracket"></i>
+                    </button>
+                </form>
+            </div>
+            
+          </div>
+        </div>
+    </div>
 
     <PageHeader header="My Profile" hidden="true"/>
 
@@ -36,10 +102,10 @@
                     </a>
                 </li>
             </ol>
-            <ol class = "inline-flex items-center space-x-1 md:space-x-3">
+            <ol class = "inline-flex items-center space-x-1 md:space-x-3 ">
                 
                 {#each navigation as navItem }
-                    <li class = "inline-flex items-center text-sm font-medium text-blue-500  dark:slate-100 dark:hover:text-slate-300">
+                    <li class = "inline-flex items-center text-sm font-medium text-slate-500 dark:slate-100 dark:hover:text-slate-300">
                         <a href={navItem.href} id="print" class="px-3 hover:text-blue-600
                         {$page.url.pathname === navItem.href ? 'underline text-blue-600' : '' }"><i class="fa-solid fa-user-pen pr-2"/>{navItem.title}</a>
                     </li>
